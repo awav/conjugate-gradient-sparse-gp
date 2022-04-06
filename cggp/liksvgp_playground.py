348b87d2dfba3f3c6f4d3fd86f844b1f2b8f676f
Artem Artemev <art.art.v@gmail.com>
2022-04-06 13:59:10 +0100

    Add inducing points plotting

--- cggp/liksvgp_playground.py
+++ cggp/liksvgp_playground.py
@@ -1,6 +1,11 @@
-from typing import Callable
+from typing import Callable, Optional
 from functools import partial
-from kmeans import kmeans_lloyd, kmeans_indices_and_distances, create_kernel_distance_fn, DistanceType
+from kmeans import (
+    kmeans_lloyd,
+    kmeans_indices_and_distances,
+    create_kernel_distance_fn,
+    DistanceType,
+)
 from models import LpSVGP
 from data import snelson1d
 import matplotlib.pyplot as plt
@@ -32,7 +37,9 @@ def create_model(data, num_inducing_points: int, distance_type: DistanceType):
     return (xt, yt), model, clustering_fn, distance_fn
 
 
-def train_using_lbfgs(data, model, clustering_fn: Callable, max_num_iters: int):
+def train_vanilla_using_lbfgs_and_standard_ip_update(
+    data, model, clustering_fn: Callable, max_num_iters: int
+):
     lbfgs = gpflow.optimizers.Scipy()
     options = dict(maxiter=max_num_iters)
     loss_fn = model.training_loss_closure(data, compile=False)
@@ -56,6 +63,66 @@ def train_using_lbfgs(data, model, clustering_fn: Callable, max_num_iters: int):
     return result
 
 
+def train_vanilla_using_lbfgs(data, model, clustering_fn: Callable, max_num_iters: int):
+    lbfgs = gpflow.optimizers.Scipy()
+    options = dict(maxiter=max_num_iters)
+    loss_fn = model.training_loss_closure(data, compile=False)
+    variables = model.trainable_variables
+
+    result = lbfgs.minimize(
+        loss_fn,
+        variables,
+        compile=True,
+        options=options,
+    )
+
+    return result
+
+
+def train_using_lbfgs_and_varpar_update(
+    data, model, clustering_fn: Callable, max_num_iters: int, distance_fn: Optional[Callable] = None
+):
+    lbfgs = gpflow.optimizers.Scipy()
+    options = dict(maxiter=max_num_iters)
+    loss_fn = model.training_loss_closure(data, compile=False)
+    variables = model.trainable_variables
+    x, y = data
+
+    def update_variational_parameters(*args, **kwargs):
+        new_iv = clustering_fn()
+
+        m = new_iv.shape[0]
+        indices, _ = kmeans_indices_and_distances(new_iv, x, distance_fn=distance_fn)
+        range_indices = tf.range(m, dtype=indices.dtype)
+        counting_map = tf.cast(range_indices[:, None] == indices[None, :], tf.int32)
+        counts = tf.reduce_sum(counting_map, axis=1, keepdims=True)
+        counts = tf.cast(counts, dtype=new_iv.dtype)
+
+        u_init = tf.zeros([m, 1], dtype=new_iv.dtype)
+        update_indices = tf.reshape(indices, [-1, 1])
+        u = tf.tensor_scatter_nd_add(u_init, update_indices, y) / counts
+        sigma2 = model.likelihood.variance
+        lambda_diag = sigma2 / counts
+
+        model.inducing_variable.Z.assign(new_iv)
+        model.q_mu.assign(u)
+        model.q_var.assign(lambda_diag)
+
+    gpflow.utilities.set_trainable(model.inducing_variable, False)
+    gpflow.utilities.set_trainable(model.q_mu, False)
+    gpflow.utilities.set_trainable(model.q_var, False)
+    update_variational_parameters()
+    result = lbfgs.minimize(
+        loss_fn,
+        variables,
+        step_callback=update_variational_parameters,
+        compile=True,
+        options=options,
+    )
+
+    return result
+
+
 if __name__ == "__main__":
     seed = 111
     np.random.seed(seed)
@@ -72,9 +139,18 @@ if __name__ == "__main__":
     x_test = np.linspace(xmin, xmax, num_test_points).reshape(-1, 1)
 
     # Model setup
-    data, model, clustering_fn, distance_fn = create_model((x, y), num_inducing_points, distance_type)
+    data, model, clustering_fn, distance_fn = create_model(
+        (x, y), num_inducing_points, distance_type
+    )
     xt, _ = data
-    result = train_using_lbfgs(data, model, clustering_fn, num_iterations)
+
+    result = train_vanilla_using_lbfgs(data, model, clustering_fn, num_iterations)
+    # result = train_vanilla_using_lbfgs_and_standard_ip_update(
+    #     data, model, clustering_fn, num_iterations
+    # )
+    # result = train_using_lbfgs_and_varpar_update(
+    #     data, model, clustering_fn, num_iterations, distance_fn=distance_fn
+    # )
 
     # Plotting
     fig, (top_ax, bottom_ax) = plt.subplots(2, 1)
@@ -93,7 +169,7 @@ if __name__ == "__main__":
         y_plot = y[centroid_mask, :]
         scatter = top_ax.scatter(x_plot, y_plot, s=8, alpha=0.5, color=color)
         top_ax.axvline(x=iv[i][0], color=color, linestyle="--")
-    
+
     # Bottom plot
     mu_test, var_test = model.predict_y(x_test)
     std_test = np.sqrt(var_test.numpy())
@@ -102,11 +178,29 @@ if __name__ == "__main__":
     up = mu_test + std_test
     down = mu_test - std_test
 
-    line = bottom_ax.plot(x_test, mu_test)[0]
+    gray = "gray"
+    line = bottom_ax.plot(x_test, mu_test, color=gray)[0]
     bottom_color = line.get_color()
     bottom_ax.fill_between(x_test.reshape(-1), up, down, color=bottom_color, alpha=0.3)
-    bottom_ax.scatter(x, y, color=bottom_color, alpha=0.5)
+    bottom_ax.scatter(x, y, color=bottom_color, alpha=0.5, s=8)
+
+    iv = model.inducing_variable.Z.numpy().reshape(-1)
+    variational_mean = model.q_mu.numpy().reshape(-1)
+    variational_variance = model.q_var.numpy().reshape(-1)
+    variational_std = np.sqrt(variational_variance)
+    scale = 1.96
+    variational_upper = variational_mean + scale * variational_std
+    variational_lower = variational_mean - scale * variational_std
+    for i in range(num_inducing_points):
+        color = colors[i]
+        x = iv[i]
+        bottom_ax.scatter(x, variational_mean[i], color=color, marker="o", s=5)
+        bottom_ax.scatter(x, variational_upper[i], color=color, marker="_")
+        bottom_ax.scatter(x, variational_lower[i], color=color, marker="_")
+        bottom_ax.plot(
+            [x, x], [variational_lower[i], variational_upper[i]], color=color, marker="_"
+        )
 
     plt.tight_layout()
     plt.show()
-
+    print("end")
