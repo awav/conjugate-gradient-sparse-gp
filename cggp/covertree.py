da6a6ed862b67e75686329242e9d08e3c49f8ddb
aterenin <aterenin@users.noreply.github.com>
2022-04-19 19:05:34 +0100

    Add modified cover tree.

--- cggp/covertree.py
+++ cggp/covertree.py
@@ -35,7 +35,7 @@ def euclid_distance(args):
 
 
 
-class OriginalCoverTreeNode:
+class CoverTreeNode:
     def __init__(
         self,
         point,
@@ -50,6 +50,17 @@ class OriginalCoverTreeNode:
         self.original_data = data.copy()
         self.children = []
 
+    def print(self, original_data):
+        plt.scatter(original_data[:, 0], original_data[:, 1], c='C1', marker='o')
+        plt.scatter(self.original_data[:, 0], self.original_data[:, 1], c= 'C2', marker = 'x')
+        plt.scatter(self.point[0], self.point[1], c='C3', marker = '+', s = 40)
+        circle = plt.Circle((self.point[0], self.point[1]), self.radius, color="blue", alpha=0.2)
+        ax = plt.gca()
+        plt.xlim([-0.5,1.5])
+        plt.ylim([-0.5,1.5])
+        ax.add_patch(circle)
+        plt.show()
+
 class OriginalCoverTree:
     def __init__(
         self,
@@ -61,38 +72,72 @@ class OriginalCoverTree:
         self.root_node = None
         self.leaf_nodes = []
         self.nodes = []
-        original_data = data
+        original_data = data.copy()
         
         point = data[0]
         distances = self.distance((point, data))
         radius = np.max(distances)
 
-        node = OriginalCoverTreeNode(point, radius, None, data)
+        node = CoverTreeNode(point, radius, None, data)
         self.root_node = node
         self.nodes.append(node)
         
-        while len(node.data) > 0:
-            plt.scatter(original_data[:, 0], original_data[:, 1], c='C1', marker='o')
-            plt.scatter(node.original_data[:, 0], node.original_data[:, 1], c= 'C2', marker = 'x')
-            plt.scatter(node.point[0], node.point[1], c='C3', marker = '+', s = 40)
-            circle = plt.Circle((node.point[0], node.point[1]), node.radius, color="blue", alpha=0.2)
-            ax = plt.gca()
-            plt.xlim([-0.5,1.5])
-            plt.ylim([-0.5,1.5])
-            ax.add_patch(circle)
-            plt.show()
-
-            point = node.data[0]
-            distances = self.distance((point, node.data))
-            radius = node.radius / 2
-            # import pdb; pdb.set_trace()
-            data = node.data[distances <= radius, :]
-            if len(data) <= 1: # or radius <= minimum_radius:
+        while len(node.data) > 0 or node.parent is not None:
+            node.print(original_data)
+            if len(node.data) == 0:
                 self.leaf_nodes.append(node)
                 node = node.parent
             else:
-                child = OriginalCoverTreeNode(point, radius, node, data)
-                self.nodes.append(child)
-                node.children.append(child)
-                node.data = node.data[distances > radius, :].copy()
-                node = child
+                point = node.data[0]
+                distances = self.distance((point, node.data))
+                radius = node.radius / 2
+                data = node.data[distances <= radius, :]
+                if len(data) <= 1: # or radius <= minimum_radius:
+                    self.leaf_nodes.append(node)
+                    node = node.parent
+                else:
+                    child = CoverTreeNode(point, radius, node, data)
+                    self.nodes.append(child)
+                    node.children.append(child)
+                    node.data = node.data[distances > radius, :].copy()
+                    node = child
+
+class ModifiedCoverTree:
+    def __init__(
+        self,
+        distance: Callable,
+        data,
+        num_levels: int,
+    ):
+        self.distance = distance
+        self.levels = [[] for _ in range(num_levels)]
+        original_data = data.copy()
+        
+        root_mean = data.mean(axis = -2)
+        root_distances = self.distance((root_mean, data))
+        max_radius = np.max(root_distances)
+
+        node = CoverTreeNode(root_mean, max_radius, None, data)
+        self.levels[0].append(node)
+
+        for level in range(1, num_levels):
+            radius = max_radius / (2 ** level)
+            for node in self.levels[level - 1]:
+                active_data = node.data.copy()
+                while len(active_data) > 0:
+                    point = active_data[0]
+                    point_distances = self.distance((point, active_data))
+                    point_neighborhood = active_data[point_distances <= radius, :]
+                    mean = point_neighborhood.mean(axis = -2)
+                    mean_distances = self.distance((mean, active_data))
+                    mean_idxs = mean_distances <= radius
+                    mean_neighborhood = active_data[mean_idxs, :]
+                    child = CoverTreeNode(mean, radius, node, mean_neighborhood)
+                    self.levels[level].append(child)
+                    node.children.append(child)
+                    active_data = active_data[~mean_idxs, :]
+        
+        self.nodes = [node for level in self.levels for node in level]
+                
+
+
