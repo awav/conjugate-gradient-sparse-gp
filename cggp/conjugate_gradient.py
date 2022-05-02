Fix the broadcasting bug in CG

cggp/conjugate_gradient.py
@@ -49,11 +49,11 @@ def conjugate_gradient(
         rz: Tensor
 
     def stopping_condition(state):
-        return (0.5 * state.rz > error_threshold) and (state.i < max_iterations)
+        return (tf.reduce_any(0.5 * state.rz > error_threshold)) and (state.i < max_iterations)
 
     def cg_step(state):
         pA = state.p @ A
-        denom = tf.reduce_sum(state.p * pA, axis=-1)
+        denom = tf.reduce_sum(state.p * pA, axis=-1, keepdims=True)
         gamma = state.rz / denom
         v = state.v + gamma * state.p
         i = state.i + 1
@@ -93,4 +93,4 @@ class CGPreconditioner:
 class EyePreconditioner:
     @abc.abstractmethod
     def __call__(self, vec: Tensor) -> Tuple[Tensor, Tensor]:
-        return vec, tf.reduce_sum(tf.square(vec))
+        return vec, tf.reduce_sum(tf.square(vec), axis=-1, keepdims=True)

cggp/logdet_playground.py
@@ -19,11 +19,12 @@ def logdet_gradient_estimation():
         return gradient
 
     def gradient_logdet_cg(matrix):
-        threshold = 0.1
+        threshold = 1e-9
         dtype = matrix.dtype
         identity = tf.linalg.diag(tf.ones(matrix.shape[0], dtype=dtype))
         initial_solution = tf.zeros(matrix.shape, dtype=dtype)
         gradient, stats = conjugate_gradient(matrix, identity, initial_solution, threshold)
+        gradient = tf.transpose(gradient)
         return gradient, stats
     
     def gradient_logdet_solve(matrix):
@@ -77,6 +78,7 @@ def logdet_gradient_estimation():
     print(f"Conjugate gradient stopped with {cg_error}")
     print(f"Max eigenvalue: {max_eig_val}")
     print(f"Min eigenvalue: {min_eig_val}")
+    print(f"Condition number: {max_eig_val / min_eig_val}")
 
     print()
 