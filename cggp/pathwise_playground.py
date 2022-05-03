Fix bugs in the likelihood computation and add Alex's suggested tests

cggp/pathwise_playground.py
@@ -3,6 +3,7 @@ from models import ClusterSVGP, PathwiseClusterSVGP
 from data import snelson1d
 import tensorflow as tf
 import numpy as np
+from numpy import newaxis
 
 from playground_util import create_model, train_using_lbfgs_and_varpar_update
 
@@ -12,7 +13,7 @@ import matplotlib.pyplot as plt
 Tensor = tf.Tensor
 
 
-if __name__ == "__main__":
+def setup():
     seed = 111
     np.random.seed(seed)
     tf.random.set_seed(seed)
@@ -21,14 +22,8 @@ if __name__ == "__main__":
     distance_type = "covariance"
     num_inducing_points = 30
     num_iterations = 1000
-    num_bases = 1234
 
     x, y = train_data
-
-    xmin = x.min() - 1.0
-    xmax = x.max() + 1.0
-    num_test_points = 100
-    x_test = np.linspace(xmin, xmax, num_test_points).reshape(-1, 1)
 
     model_class = ClusterSVGP
     data, experimental_model, clustering_fn, distance_fn = create_model(
@@ -54,43 +49,98 @@ if __name__ == "__main__":
         pseudo_u=experimental_model.pseudo_u,
     )
 
-    xt, yt = data
+    return data, experimental_model, pathwise_model
 
-    #######
-    ## Test
 
-    num_samples = 1111
+def test_likelihood_terms():
+    num_bases = 3333
+    num_samples = 5555
+
+    data, experimental_model, pathwise_model = setup()
+    x, y = data
+
+    def compute_expectation_samples(
+        samples,
+        y,
+    ):
+        error_squared = tf.square(y[newaxis, ...] - samples)
+        expected_error_squared = tf.reduce_mean(error_squared, axis=0)
+        return expected_error_squared
+
+    def compute_expectation_analytic(
+        q_mu,
+        q_var,
+        y,
+    ):
+        expected_f_sq = q_var + q_mu**2
+        expected_error_squared = y**2 - (2 * y * q_mu) + expected_f_sq
+        return expected_error_squared
+
+    def compute_expected_likelihood_term(model, q_mu, q_var, y):
+        N = y.shape[0]
+        noise = model.likelihood.variance
+        noise_inv = tf.math.reciprocal(noise)
+        expected_error_squared = compute_expectation_analytic(q_mu, q_var, y)
+        likelihood_term = tf.reduce_sum(0.5 * noise_inv * expected_error_squared)
+
+        constant_term = 0.5 * N * tf.math.log(2 * np.pi * noise)
+        return -(likelihood_term + constant_term)
+
     likelihood = pathwise_model.compute_likelihood_term(
         data,
         num_bases=num_bases,
         num_samples=num_samples,
     )
 
-    q_mu_x, q_var_x = experimental_model.predict_f(xt)
-    likelihoods = experimental_model.likelihood.variational_expectations(q_mu_x, q_var_x, yt)
-    likelihood_ground_truth = tf.reduce_sum(likelihoods)
+    q_mu_x, q_var_x = experimental_model.predict_f(x)
+    likelihoods = experimental_model.likelihood.variational_expectations(q_mu_x, q_var_x, y)
+    likelihood_ground_truth_gpflow = tf.reduce_sum(likelihoods)
+
+    likelihood_ground_truth_sqmean = compute_expected_likelihood_term(
+        experimental_model, q_mu_x, q_var_x, y
+    )
 
     print(f"Likelihood term estimated: {likelihood.numpy()}")
-    print(f"Likelihood term expected: {likelihood_ground_truth.numpy()}")
+    print(f"Likelihood term expected (gpflow): {likelihood_ground_truth_gpflow.numpy()}")
+    print(f"Likelihood term expected (sqmean): {likelihood_ground_truth_sqmean.numpy()}")
+
+    ########
     # np.testing.assert_allclose(likelihood, likelihood_ground_truth)
 
-    ###########
-    ## Plotting
+    samples = pathwise_model.pathwise_samples(x, num_bases, num_samples)
+    q_mu_x, q_var_x = experimental_model.predict_f(x)
 
+    likelihood_samples = compute_expectation_samples(samples, y)
+    likelihood_analytic = compute_expectation_analytic(q_mu_x, q_var_x, y)
+
+    print(f"Expectation estimated: {likelihood_samples.numpy().sum()}")
+    print(f"Expectation analytic: {likelihood_analytic.numpy().sum()}")
+
+
+def plotting_samples():
+    data, experimental_model, pathwise_model = setup()
+    x, y = data
+    xmin = x.min() - 1.0
+    xmax = x.max() + 1.0
+    num_test_points = 100
+    x_test = np.linspace(xmin, xmax, num_test_points).reshape(-1, 1)
+
+    num_bases = 1234
     num_samples = 2
     samples = pathwise_model.pathwise_samples(x_test, num_bases, num_samples)
     samples_expected = experimental_model.predict_f_samples(x_test, num_samples=num_samples)
 
     blue = "tab:blue"
     gray = "tab:gray"
+    orange = "tab:orange"
 
     fig, ax = plt.subplots(1, 1)
     samples = samples.numpy().squeeze().T
     samples_expected = samples_expected.numpy().squeeze().T
 
     ax.set_xlim(x_test.min(), x_test.max())
-    ax.plot(x_test, samples, color="tab:blue")
-    ax.plot(x_test, samples_expected, color="tab:orange")
+    ax.plot(x_test, samples, color=blue)
+    ax.plot(x_test, samples_expected, color=orange)
 
     f_mu, f_var = experimental_model.predict_f(x_test)
     f_std = np.sqrt(f_var.numpy())
@@ -106,5 +156,8 @@ if __name__ == "__main__":
 
     plt.tight_layout()
     plt.show()
+    pass
 
-    print()
+
+if __name__ == "__main__":
+    test_likelihood_terms()

cggp/playground_util.py
@@ -1,3 +1,4 @@
+from ast import Call
 from typing import Callable, Optional, TypeVar
 from functools import partial
 from kmeans import (
@@ -95,6 +96,28 @@ def train_vanilla_using_lbfgs(
     return result
 
 
+def update_inducing_parameters(model, data, distance_fn: Optional[Callable], clustering_fn: Callable) -> None:
+    x, y = data
+    new_iv = clustering_fn()
+
+    m = new_iv.shape[0]
+    indices, _ = kmeans_indices_and_distances(new_iv, x, distance_fn=distance_fn)
+    range_indices = tf.range(m, dtype=indices.dtype)
+    counting_map = tf.cast(range_indices[:, None] == indices[None, :], tf.int32)
+    counts = tf.reduce_sum(counting_map, axis=1, keepdims=True)
+    counts = tf.cast(counts, dtype=new_iv.dtype)
+
+    u_init = tf.zeros([m, 1], dtype=new_iv.dtype)
+    update_indices = tf.reshape(indices, [-1, 1])
+    u = tf.tensor_scatter_nd_add(u_init, update_indices, y) / counts
+    sigma2 = model.likelihood.variance
+    lambda_diag = sigma2 / counts
+
+    model.inducing_variable.Z.assign(new_iv)
+    model.pseudo_u.assign(u)
+    model.diag_variance.assign(lambda_diag)
+
+
 def train_using_lbfgs_and_varpar_update(
     data,
     model: ClusterSVGP,
@@ -106,27 +129,9 @@ def train_using_lbfgs_and_varpar_update(
     options = dict(maxiter=max_num_iters)
     loss_fn = model.training_loss_closure(data, compile=False)
     variables = model.trainable_variables
-    x, y = data
 
     def update_variational_parameters(*args, **kwargs):
-        new_iv = clustering_fn()
-
-        m = new_iv.shape[0]
-        indices, _ = kmeans_indices_and_distances(new_iv, x, distance_fn=distance_fn)
-        range_indices = tf.range(m, dtype=indices.dtype)
-        counting_map = tf.cast(range_indices[:, None] == indices[None, :], tf.int32)
-        counts = tf.reduce_sum(counting_map, axis=1, keepdims=True)
-        counts = tf.cast(counts, dtype=new_iv.dtype)
-
-        u_init = tf.zeros([m, 1], dtype=new_iv.dtype)
-        update_indices = tf.reshape(indices, [-1, 1])
-        u = tf.tensor_scatter_nd_add(u_init, update_indices, y) / counts
-        sigma2 = model.likelihood.variance
-        lambda_diag = sigma2 / counts
-
-        model.inducing_variable.Z.assign(new_iv)
-        model.pseudo_u.assign(u)
-        model.diag_variance.assign(lambda_diag)
+        update_inducing_parameters(model, data, distance_fn, clustering_fn)
 
     gpflow.utilities.set_trainable(model.inducing_variable, False)
     use_jit = True  # TODO(awav): resolve the problem with recompiling in Scipy