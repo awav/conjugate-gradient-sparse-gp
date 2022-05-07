Fix a compilation issue in the conjugate gradient

cggp/cli_utils.py
@@ -16,8 +16,7 @@ def create_model(
     data,
     num_inducing_points: int,
 ):
-    x, y = data
-
+    x = np.array(data[0])
     n = x.shape[0]
     dim = x.shape[-1]
     default_variance = 0.1

cggp/conjugate_gradient.py
@@ -7,14 +7,28 @@ import tensorflow as tf
 Tensor = tf.Tensor
 
 
+class CGState(NamedTuple):
+    """
+    Args:
+        i: Iteration index
+        v: Solution vector(s)
+    """
+
+    i: Tensor
+    v: Tensor
+    r: Tensor
+    p: Tensor
+    rz: Tensor
+
+
 def conjugate_gradient(
     matrix: Tensor,
     rhs: Tensor,
     initial_solution: Tensor,
     error_threshold: float,
+    preconditioner: Optional[Callable] = None,
     max_iterations: Optional[int] = None,
     max_steps_cycle: int = 100,
-    preconditioner: Optional[Callable] = None,
 ):
     """
     Conjugate gradient for solving system of linear equations math:`Av = b`.
@@ -27,27 +41,15 @@ def conjugate_gradient(
         error_threshold:
     """
 
+    if preconditioner is None:
+        preconditioner = EyePreconditioner()
+
     if max_iterations is None:
         max_iterations = tf.shape(matrix)[0]
-
-    preconditioner = EyePreconditioner() if preconditioner is None else preconditioner
 
     A = matrix
     v = initial_solution
     b = rhs
-
-    class CGState(NamedTuple):
-        """
-        Args:
-            i: Iteration index
-            v: Solution vector(s)
-        """
-
-        i: Tensor
-        v: Tensor
-        r: Tensor
-        p: Tensor
-        rz: Tensor
 
     def stopping_condition(state):
         larger_threshold = tf.reduce_any(0.5 * state.rz > error_threshold)
@@ -92,7 +94,7 @@ class CGPreconditioner:
         raise NotImplementedError
 
 
-class EyePreconditioner:
+class EyePreconditioner(CGPreconditioner):
     @abc.abstractmethod
     def __call__(self, vec: Tensor) -> Tuple[Tensor, Tensor]:
         return vec, tf.reduce_sum(tf.square(vec), axis=-1, keepdims=True)
@@ -133,15 +135,16 @@ class ConjugateGradient:
             max_steps_cycle = max_iterations + 1  # Make sure that we don't run it in the end of CG
 
         preconditioner = self.preconditioner
+        error_threshold = self.error_threshold
 
         solution, stats = conjugate_gradient(
             matrix,
             rhs,
             initial_solution,
-            self.error_threshold,
-            max_iterations,
-            max_steps_cycle,
-            preconditioner,
+            error_threshold,
+            preconditioner=preconditioner,
+            max_iterations=max_iterations,
+            max_steps_cycle=max_steps_cycle,
         )
 
         return solution

cggp/data.py
@@ -8,6 +8,7 @@ from io import BytesIO
 import urllib.request
 
 import bayesian_benchmarks.data as bbd
+import tensorflow as tf
 from gpflow.config import default_float
 
 Dataset = Tuple[np.ndarray, np.ndarray]
@@ -71,7 +72,7 @@ def norm_dataset(data: Dataset) -> Dataset:
     return norm(data[0]), norm(data[1])
 
 
-def load_data(name: str) -> DatasetBundle:
+def load_data(name: str, as_tensor: bool = False) -> DatasetBundle:
     if name == "snelson1d":
         train, test = snelson1d("~/.dataset/snelson1d/")
     else:
@@ -85,11 +86,17 @@ def load_data(name: str) -> DatasetBundle:
     (x_train, x_mu, x_std), (y_train, y_mu, y_std) = norm_dataset(train)
     x_test = (test[0] - x_mu) / x_std
     y_test = (test[1] - y_mu) / y_std
-    return DatasetBundle(name,
-        (_to_float(x_train), _to_float(y_train)),
-        (_to_float(x_test), _to_float(y_test)),
-    )
+
+    x_train = _to_float(x_train, as_tensor=as_tensor)
+    y_train = _to_float(y_train, as_tensor=as_tensor)
+    x_test = _to_float(x_test, as_tensor=as_tensor)
+    y_test = _to_float(y_test, as_tensor=as_tensor)
+
+    return DatasetBundle(name, (x_train, y_train), (x_test, y_test))
 
 
-def _to_float(arr: np.ndarray):
-    return arr.astype(default_float())
+def _to_float(arr: np.ndarray, as_tensor: bool):
+    dtype = default_float()
+    if as_tensor:
+        return tf.convert_to_tensor(arr, dtype=dtype)
+    return arr.astype(dtype)