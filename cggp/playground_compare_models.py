8cd64f667ce10a01b321ae005fdbf05d59f7b322
Artem Artemev <art.art.v@gmail.com>
2022-05-13 15:12:53 +0100

    Temporary changes

--- cggp/playground_compare_models.py
+++ cggp/playground_compare_models.py
@@ -105,46 +105,46 @@ if __name__ == "__main__":
         monitor=monitor_cggp,
     )
 
-    logdir_clustergp = f"{logdir}/clustergp"
-    monitor_clustergp = create_monitor(
-        clustergp,
-        train_data,
-        test_data,
-        monitor_batch_size,
-        use_tensorboard=use_tb,
-        logdir=logdir_clustergp,
-    )
-    train_using_adam_and_update(
-        train_data,
-        clustergp,
-        num_iterations,
-        batch_size,
-        learning_rate,
-        update_fn=clustergp_update_fn,
-        update_during_training=True,
-        use_jit=use_jit,
-        monitor=monitor_clustergp,
-    )
+    # logdir_clustergp = f"{logdir}/clustergp"
+    # monitor_clustergp = create_monitor(
+    #     clustergp,
+    #     train_data,
+    #     test_data,
+    #     monitor_batch_size,
+    #     use_tensorboard=use_tb,
+    #     logdir=logdir_clustergp,
+    # )
+    # train_using_adam_and_update(
+    #     train_data,
+    #     clustergp,
+    #     num_iterations,
+    #     batch_size,
+    #     learning_rate,
+    #     update_fn=clustergp_update_fn,
+    #     update_during_training=True,
+    #     use_jit=use_jit,
+    #     monitor=monitor_clustergp,
+    # )
 
-    logdir_lpsvgp = f"{logdir}/lpsvgp"
-    monitor_lpsvgp = create_monitor(
-        lpsvgp,
-        train_data,
-        test_data,
-        monitor_batch_size,
-        use_tensorboard=use_tb,
-        logdir=logdir_lpsvgp,
-    )
-    train_using_adam_and_update(
-        train_data,
-        lpsvgp,
-        num_iterations,
-        batch_size,
-        learning_rate,
-        update_fn=None,
-        update_during_training=False,
-        use_jit=use_jit,
-        monitor=monitor_lpsvgp,
-    )
+    # logdir_lpsvgp = f"{logdir}/lpsvgp"
+    # monitor_lpsvgp = create_monitor(
+    #     lpsvgp,
+    #     train_data,
+    #     test_data,
+    #     monitor_batch_size,
+    #     use_tensorboard=use_tb,
+    #     logdir=logdir_lpsvgp,
+    # )
+    # train_using_adam_and_update(
+    #     train_data,
+    #     lpsvgp,
+    #     num_iterations,
+    #     batch_size,
+    #     learning_rate,
+    #     update_fn=None,
+    #     update_during_training=False,
+    #     use_jit=use_jit,
+    #     monitor=monitor_lpsvgp,
+    # )
 
     print(f"End. Check tensorboard logdir {logdir}")
