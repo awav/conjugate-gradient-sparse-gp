12336e31a54c1c1666676138b827010520db9e73
Artem Artemev <art.art.v@gmail.com>
2022-06-08 14:32:42 +0100

    Formatting

--- cggp/paper_africa_compare_models.py
+++ cggp/paper_africa_compare_models.py
@@ -8,10 +8,9 @@ from gpflow.utilities import parameter_dict
 from utils import store_logs, to_numpy, jit
 from pathlib import Path
 
-from cli_utils import create_model_and_kmeans_update_fn, create_model_and_covertree_update_fn
+from cli_utils import create_model_and_covertree_update_fn
 from optimize import (
     train_using_adam_and_update,
-    train_using_lbfgs_and_update,
     create_monitor,
     transform_to_dataset,
 )
