2c7c1b6000c6de80920261288017f48b536ffbe1
aterenin <aterenin@users.noreply.github.com>
2022-08-17 18:58:07 +0100

    Fix Voronoi repartitioning.

--- cggp/playground_covertree.py
+++ cggp/playground_covertree.py
@@ -11,7 +11,6 @@ if __name__ == "__main__":
     x = np.random.rand(1024*16, 2)
     y = np.random.rand(1024*16, 1)
     data = x, y
-    np.savetxt(f"ct_x.csv", x, delimiter=",")
     start = time.time()
     tree = CoverTree(euclid_distance, data, spatial_resolution=0.18/16)
     print(f"Total time: {time.time() - start}")
@@ -27,16 +26,21 @@ if __name__ == "__main__":
         ax.set_ylim([-0.125, 1.125])
         ax.axis('equal')
         ax.set_title(f"$R_{level} = {radius}$, $\delta_{level} = {separation:.02g}$, $\\frac{{R_{level}}}{{\delta_{level}}} = {ratio:.02f}$")
-        for node in tree.levels[level]:
+        for (idx,node) in enumerate(tree.levels[level]):
             point = node.point
-            node_data, _ = node.original_data
+            node_data, _ = node.plotting_data
+            if level > 0: np.savetxt(f"ct_L{level}_X{idx}.csv", node.plotting_data[0], delimiter=",")
+            if level > 0: np.savetxt(f"ct_L{level}_V{idx}.csv", node.voronoi_data[0], delimiter=",")
             ax.scatter(node_data[:, 0], node_data[:, 1], alpha=0.1)
             ax.scatter(point[0], point[1], c="white", marker="o", edgecolors="black", s=10)
             circle = plt.Circle((point[0], point[1]), radius, color="C0", alpha=0.1)
             ax.add_patch(circle)
-        out = np.array([np.concatenate((node.point,np.array([node.radius]),node.parent.point if node.parent is not None else ())) for node in tree.levels[level]])
-        if level > 0:
-            np.savetxt(f"ct_L{level}.csv", out, delimiter=",")
+        if level > 0: np.savetxt(f"ct_L{level}.csv", np.array([node.point for node in tree.levels[level]]), delimiter=",")
+    if level > 0: np.savetxt("ct_r.csv", np.array([nodes[0].radius for nodes in tree.levels])[1:])
+
+    for (idx,nodes) in enumerate(tree.levels):
+        print(f"L{idx}X: ", np.concatenate([node.plotting_data[0] for node in nodes], axis=-2).size)
+        print(f"L{idx}V: ", np.concatenate([node.voronoi_data[0] for node in nodes], axis=-2).size)
 
     print(f"Saving, total time: {time.time() - start}")
     if x.size < 512:
