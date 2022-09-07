2064b40ae3ed54f27856b2007ba12377f7469d39
Artem Artemev <art.art.v@gmail.com>
2022-09-07 10:25:34 +0100

    Format covertree.py

--- cggp/covertree.py
+++ cggp/covertree.py
@@ -62,7 +62,7 @@ class CoverTree:
             root.plotting_data = (root.data[0].copy(), root.data[1].copy())
         self.levels = [[] for _ in range(num_levels)]
         self.levels[0].append(root)
-        neighbor_factor = 4 * (1 - 1/2**np.arange(num_levels, -1, -1)) 
+        neighbor_factor = 4 * (1 - 1 / 2 ** np.arange(num_levels, -1, -1))
 
         for level in range(1, num_levels):
             radius = max_radius / (2**level)
@@ -110,7 +110,8 @@ class CoverTree:
                     child.r_neighbors = [
                         r_neighbor
                         for r_neighbor in potential_child_r_neighbors
-                        if self.distance((r_neighbor.point, child.point)) <= neighbor_factor[level] * radius
+                        if self.distance((r_neighbor.point, child.point))
+                        <= neighbor_factor[level] * radius
                     ]
                     if plotting:
                         child.plotting_data = (child.data[0].copy(), child.data[1].copy())
