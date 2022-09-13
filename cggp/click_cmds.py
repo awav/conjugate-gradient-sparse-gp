cfd4199049a48f22ea14bb5b2c4bd099a843f0ed
Artem Artemev <art.art.v@gmail.com>
2022-09-13 12:01:20 +0100

    Add greedy selection

--- cggp/click_cmds.py
+++ cggp/click_cmds.py
@@ -42,7 +42,7 @@ def kmeans(ctx: click.Context, max_num_ip: int, distance_type: DistanceType):
     common_ctx: Dict = ctx_obj["common_ctx"]
 
     clustering_type = "kmeans"
-    clustering_kwargs = {"num_inducing_points": max_num_ip}
+    clustering_kwargs = {"max_points": max_num_ip}
 
     model, update_fn = create_model_and_update_fn(
         common_ctx["model_class_fn"],
@@ -101,6 +101,35 @@ def uniform(ctx: click.Context, max_num_ip: int, distance_type: DistanceType):
     common_ctx: Dict = ctx_obj["common_ctx"]
 
     clustering_type = "uniform"
+    clustering_kwargs = {"max_points": max_num_ip}
+
+    model, update_fn = create_model_and_update_fn(
+        common_ctx["model_class_fn"],
+        common_ctx["dataset"].train,
+        clustering_type=clustering_type,
+        distance_type=distance_type,
+        use_jit=common_ctx["jit"],
+        clustering_kwargs=clustering_kwargs,
+    )
+
+    ctx_obj["ip_ctx"] = dict(
+        model=model,
+        update_fn=update_fn,
+        clustering_type=clustering_type,
+        clustering_kwargs=clustering_kwargs,
+        distance_type=distance_type,
+    )
+
+
+@click.group("greedy")
+@click.option("-m", "--max-num-ip", type=int)
+@click.option("-d", "--distance-type", type=DistanceChoices, default="euclidean")
+@click.pass_context
+def greedy(ctx: click.Context, max_num_ip: int, distance_type: DistanceType):
+    ctx_obj: Dict = ctx.obj
+    common_ctx: Dict = ctx_obj["common_ctx"]
+
+    clustering_type = "greedy"
     clustering_kwargs = {"max_points": max_num_ip}
 
     model, update_fn = create_model_and_update_fn(
--- cggp/selection.py
+++ cggp/selection.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 import gpflow
 from typing import Callable, Optional, Tuple
 import numpy as np
@@ -77,30 +78,75 @@ def oips(kernel: gpflow.kernels.Kernel, inputs: Tensor, rho: float, max_points: 
     kxx = kernel(inputs, full_cov=False)
     i = tf.math.argmax(kxx)
     inducing_points = tf.convert_to_tensor([inputs[i]])
+    indices = tf.convert_to_tensor([i])
     inputs = tf.concat([inputs[:i], inputs[i:]], axis=0)
 
-    def cond(i, j, _):
+    def cond(i, j, _inducing_points, _indices):
         return i < n and j < max_points
 
-    def body(i, j, inducing_points):
+    def body(i, j, inducing_points, indices):
         point = inputs[i : i + 1]
         kix = kernel(point, inducing_points)
         weight = tf.math.reduce_max(kix)
         if weight < rho:
-            inducing_points = tf.concat([inducing_points, point], axis=0)
-            return [i + 1, j + 1, inducing_points]
-        return [i + 1, j, inducing_points]
+            new_inducing_points = tf.concat([inducing_points, point], axis=0)
+            new_indices = tf.concat([indices, [i]], axis=0)
+            return [i + 1, j + 1, new_inducing_points, new_indices]
+        return [i + 1, j, inducing_points, indices]
 
     i0 = tf.constant(1)
     j0 = tf.constant(1)
     initial_state = [i0, j0, inducing_points]
-    shapes = [i0.shape, j0.shape, tf.TensorShape([None, inputs.shape[-1]])]
+    shapes = [i0.shape, j0.shape, tf.TensorShape([None, inputs.shape[-1]]), tf.TensorShape([None])]
     result = tf.while_loop(cond, body, initial_state, shape_invariants=shapes)
     return result[-1]
 
 
-def uniform(inputs: Tensor, max_points: int) -> Tensor:
+def uniform(inputs: Tensor, max_points: int) -> Tuple[Tensor, Tensor]:
     max_value = tf.shape(inputs)[0]
     indices = tf.random.uniform([max_points], maxval=max_value, dtype=max_value.dtype)
     sample = tf.gather(inputs, indices, axis=0)
-    return sample
+    return sample, indices
+
+
+def greedy_selection(kernel, inputs: Tensor, max_points: int) -> Tuple[Tensor, Tensor]:
+    n = tf.shape(inputs)[0]
+    m = n if max_points > n else max_points
+
+    perm = tf.random.shuffle(tf.range(n))
+    X = tf.gather(inputs, perm)  # shuffle training data
+    di = kernel(X, full_cov=False)  #  diagonal entries of kernel
+    inds = tf.math.argmax(di)[None]  # select first point, point with highest variance
+    ci = tf.zeros([1, n], dtype=inputs.dtype)
+
+    State = namedtuple("State", "m, inds, di, ci")
+    size = tf.cast(n, di.dtype)
+
+    def stopping_criterion(state):
+        return state.m < m
+
+    def loop_body(state):
+        j = gpflow.utilities.to_default_int(state.inds[-1])  # index in X of last point chose
+        new_Z = X[j : j + 1]  # input value of last point chosen
+        dj = tf.math.sqrt(state.di[j])  # conditional standard deviation of point chosen
+        cj = state.ci[: state.m, j : j + 1]  # [m, 1]
+        K = kernel(X, new_Z, full_cov=True)  # [n, 1],  covariance between new point and all points
+        ei = (K - tf.matmul(state.ci, cj, transpose_a=True)) / dj  # [n,1]
+        new_ci = tf.concat([state.ci, tf.transpose(ei)], axis=0)
+        new_di = state.di - tf.square(ei)[:, 0]
+        new_inds = tf.concat([state.inds, [tf.math.argmax(new_di)]], axis=0)
+        new_state = State(state.m + 1, new_inds, new_di, new_ci)
+        return [new_state]
+
+    init_state = [State(1, inds, di, ci)]
+    tshape = tf.TensorShape
+
+    shape_invariants = [State(tshape([]), tshape([None]), tshape([None]), tshape([None, None]))]
+
+    final_state = tf.while_loop(
+        stopping_criterion, loop_body, init_state, shape_invariants=shape_invariants
+    )
+    final_state = tf.nest.map_structure(tf.stop_gradient, final_state)
+    perm_inds = tf.gather(perm, final_state[0].inds)
+    Z = tf.gather(inputs, perm_inds)
+    return Z, perm_inds
